edc4a3875b2c5c9aaecebbd1f84b7666fb2f79e4
Ali Najafi K. <147445558+pureAliNajafi@users.noreply.github.com>
2024-09-10 16:04:18 +0330

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -21,18 +21,4 @@ export const auth = getAuth(app);
 // Export the app if needed
 export default app;
 
-/* import firebase from "firebase/app";
-import "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-};
-const app = firebase.initializeApp(firebaseConfig);
-export const auth = app.auth();
-export default app;
- */
